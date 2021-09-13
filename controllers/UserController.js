b68f33f0030809eee34418aae8fab76b10af4662
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-09-13 15:54:38 -0300

    file reader - add fn salvar foto

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,12 @@
 class UserController {
 
     constructor(formId, tableId) {
-        this.formEl = document.getElementById(formId);
-        this.tableId = document.getElementById(tableId);
 
-        this.onSubmit();
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit()
+
     }
 
     onSubmit() {
@@ -12,15 +14,49 @@ class UserController {
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
-            this.addLine(this.getValues());
+
+            let values = this.getValues();
+
+            this.getPhoto((content) => {
+
+                values.photo = content;
+
+                this.addLine(values);
+            });
+
 
         });
-    } //close onSubmit
+
+    }
+
+    getPhoto(callback) {
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+
+            if (item.name === 'photo') {
+                return item;
+            }
+
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
+
+    }
 
     getValues() {
 
         let user = {};
-        
+
         [...this.formEl.elements].forEach(function (field, index) {
 
             if (field.name === "gender") {
@@ -48,15 +84,14 @@ class UserController {
             user.admin
         );
 
-    } // close getValues
+    }
+
 
     addLine(dataUser) {
 
-        console.log(dataUser);
-
-        document.getElementById(tableId).innerHTML = `
+        this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
@@ -68,5 +103,7 @@ class UserController {
             </tr>
         `;
 
-    } // close AddLine
+    }
+
+
 }
