2e8e90ff09b941a5526119d4a25aeeabb5bde6a6
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-09-13 19:48:58 -0300

    ajustes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,41 +15,61 @@ class UserController {
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]")
+            btn.disabled = true;
+
             let values = this.getValues();
 
-            this.getPhoto((content) => {
+            this.getPhoto().then(
+                (content) => {
+                    values.photo = content;
 
-                values.photo = content;
+                    this.addLine(values);
 
-                this.addLine(values);
-            });
+                    this.formEl.reset();
 
-
+                    btn.disabled = false;
+                },
+                (e) => {
+                    console.error(e);
+                }
+            );
         });
-
     }
 
-    getPhoto(callback) {
+    getPhoto() {
 
-        let fileReader = new FileReader();
+        return new Promise((resolve, reject) => {
 
-        let elements = [...this.formEl.elements].filter(item => {
+            let fileReader = new FileReader();
 
-            if (item.name === 'photo') {
-                return item;
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+                reject(e);
+            }
+
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
             }
 
         });
-
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-
-            callback(fileReader.result);
-
-        };
-
-        fileReader.readAsDataURL(file);
 
     }
 
@@ -64,6 +84,9 @@ class UserController {
                 if (field.checked) {
                     user[field.name] = field.value
                 }
+
+            } else if (field.name == "admin") {
+                user[field.name] = field.checked;
 
             } else {
 
@@ -89,21 +112,27 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
         `;
 
+        this.tableEl.appendChild(tr);
+
+
     }
+
+
+    
+
 
 
 }
